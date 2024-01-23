77a7a3671bc0306839a2d942d2e0e459eda5e62f
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2024-01-23 21:37:40 +0800

    fix style

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -7,8 +7,8 @@
         <mdui-menu>
             <mdui-menu-item style="display: flex; width: 100%; align-items: center; justify-content: center;">Hi, {{
                 $attrs.config.userName || "Guest" }}</mdui-menu-item>
-            <mdui-menu-item><mdui-checkbox :checked="$attrs.useJBM" @input="$attrs.changeFont()">Use JetBrains Mono</mdui-checkbox></mdui-menu-item>
-            <mdui-menu-item><mdui-checkbox :checked="$attrs.showClock" @input="$attrs.toggleClock()">Show Clock</mdui-checkbox></mdui-menu-item>
+            <mdui-menu-item @click="$attrs.changeFont()"><mdui-checkbox :checked="$attrs.useJBM" @input="$attrs.changeFont()">Use JetBrains Mono</mdui-checkbox></mdui-menu-item>
+            <mdui-menu-item @click="$attrs.toggleClock()"><mdui-checkbox :checked="$attrs.showClock" @input="$attrs.toggleClock()">Show Clock</mdui-checkbox></mdui-menu-item>
             <mdui-menu-item @click="$attrs.openSetConfigDialog()">Settings</mdui-menu-item>
         </mdui-menu>
     </mdui-dropdown>
